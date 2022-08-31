6292d86e4d9c09602c2ad9a2a25c9e0c90c1a21e
HAmid <hamidprince823@gmail.com>
2022-08-31 16:42:16 +0500

    images

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html"],
   theme: {
     screens: {
       sm: "480px",
       md: "769px",
       lg: "976px",
       xl: "1440px",
+    }, 
+    extend: {
+      backgroundImage :{
+        'dubai':" url('../img/dubai-tower.jpeg')",
+        'london':" url('../img/london.jpg')",
+        'paris':" url('../img/paris.jpg')",
+        'grl':" linear-gradient(to right, #4364F7,#6FB1FC)",
+         'white':" linear-gradient(to top, #6FB1FC,#6FB1FC)",
+         'whiteBLue':" linear-gradient(to right, #6FB1FC,#4364F7)",
+
+      }
     },
-    extend: {},
   },
   plugins: [],
 };
