91e9d60d81bbe49e76ce2fc252318d41b27fef12
HAmid <hamidprince823@gmail.com>
2022-09-01 15:42:13 +0500

    tailwind configration

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,17 +7,17 @@ module.exports = {
       md: "769px",
       lg: "976px",
       xl: "1440px",
-    }, 
+    },
     extend: {
-      backgroundImage :{
-        'dubai':" url('../img/dubai-tower.jpeg')",
-        'london':" url('../img/london.jpg')",
-        'paris':" url('../img/paris.jpg')",
-        'grl':" linear-gradient(to right, #4364F7,#6FB1FC)",
-         'white':" linear-gradient(to top, #6FB1FC,#6FB1FC)",
-         'whiteBLue':" linear-gradient(to right, #6FB1FC,#4364F7)",
-
-      }
+      backgroundImage: {
+        dubai: " url('../img/dubai-tower.jpeg')",
+        london: " url('../img/london.jpg')",
+        paris: " url('../img/paris.jpg')",
+        formBg: " url('../img/form-img.png')",
+        grl: " linear-gradient(to right, #4364F7,#6FB1FC)",
+        white: " linear-gradient(to top, #6FB1FC,#6FB1FC)",
+        whiteBLue: " linear-gradient(to right, #6FB1FC,#4364F7)",
+      },
     },
   },
   plugins: [],
